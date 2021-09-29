7f5b47a6309f4097be16c677670380f9c66c4d4d
antstalepresh <stalepresh121@outlook.com>
2021-09-30 03:27:33 +1000

    resolve proposal codec registration

--- x/bech32ibc/types/codec.go
+++ x/bech32ibc/types/codec.go
@@ -3,12 +3,18 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 )
 
 func RegisterCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&UpdateHrpIbcChannelProposal{}, "osmosis/UpdateHrpIbcChannelProposal", nil)
 }
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
+	registry.RegisterImplementations(
+		(*govtypes.Content)(nil),
+		&UpdateHrpIbcChannelProposal{},
+	)
 }
 
 var (
