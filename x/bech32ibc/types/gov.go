2e57443d5f12d6ae4534c33cfb05afe3f6b5e305
antstalepresh <stalepresh121@outlook.com>
2021-10-13 00:36:53 +1000

    add validateBasic for UpdateHrpIbcChannelProposal

--- x/bech32ibc/types/gov.go
+++ x/bech32ibc/types/gov.go
@@ -45,6 +45,9 @@ func (p *UpdateHrpIbcChannelProposal) ValidateBasic() error {
 	if err != nil {
 		return err
 	}
+	if p.IcsToHeightOffset == 0 && p.IcsToTimeOffset == 0 {
+		return ErrInvalidOffsetHeightTimeout
+	}
 	return ValidateHrp(p.Hrp)
 }
 
