928ac4842851d2d107bbdfceea43c9a194836084
antstalepresh <stalepresh121@outlook.com>
2021-11-23 22:49:32 +1000

    enable docker test

--- go.mod
+++ go.mod
@@ -4,21 +4,24 @@ go 1.15
 
 require (
 	github.com/armon/go-metrics v0.3.9
-	github.com/cosmos/cosmos-sdk v0.44.2
+	github.com/cosmos/cosmos-sdk v0.44.3
 	github.com/cosmos/ibc-go v1.2.2
+	github.com/cosmos/relayer v1.0.0 // indirect
 	github.com/gogo/protobuf v1.3.3
 	github.com/golang/protobuf v1.5.2
 	github.com/gorilla/mux v1.8.0
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0
-	github.com/spf13/cast v1.3.1
+	github.com/spf13/cast v1.4.1
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/objx v0.3.0 // indirect
 	github.com/stretchr/testify v1.7.0
-	github.com/tendermint/tendermint v0.34.13
+	github.com/tendermint/tendermint v0.34.14
 	github.com/tendermint/tm-db v0.6.4
 	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c
 	google.golang.org/grpc v1.40.0
+	gopkg.in/airbrake/gobrake.v2 v2.0.9 // indirect
+	gopkg.in/gemnasium/logrus-airbrake-hook.v2 v2.1.2 // indirect
 )
 
 replace google.golang.org/grpc => google.golang.org/grpc v1.33.2
--- test/test_chains.go
+++ test/test_chains.go
@@ -1,93 +1,93 @@
 package test
 
-// import (
-// 	"fmt"
-// 	"testing"
-// 	"time"
+import (
+	"fmt"
+	"testing"
+	"time"
 
-// 	"github.com/cosmos/cosmos-sdk/server"
-// 	"github.com/stretchr/testify/require"
+	"github.com/cosmos/relayer/relayer"
 
-// 	ry "github.com/cosmos/relayer/relayer"
-// )
+	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/stretchr/testify/require"
+)
 
-// const (
-// 	// SEED1 is a mnenomic
-// 	//nolint:lll
-// 	SEED1 = "cake blossom buzz suspect image view round utility meat muffin humble club model latin similar glow draw useless kiwi snow laugh gossip roof public"
-// 	// SEED2 is a mnemonic
-// 	//nolint:lll
-// 	SEED2 = "near little movie lady moon fuel abandon gasp click element muscle elbow taste indoor soft soccer like occur legend coin near random normal adapt"
-// )
+const (
+	// SEED1 is a mnenomic
+	//nolint:lll
+	SEED1 = "cake blossom buzz suspect image view round utility meat muffin humble club model latin similar glow draw useless kiwi snow laugh gossip roof public"
+	// SEED2 is a mnemonic
+	//nolint:lll
+	SEED2 = "near little movie lady moon fuel abandon gasp click element muscle elbow taste indoor soft soccer like occur legend coin near random normal adapt"
+)
 
-// var (
-// 	// GAIA BLOCK TIMEOUTS are located in the gaia setup script in the
-// 	// setup directory.
-// 	// timeout_commit = "1000ms"
-// 	// timeout_propose = "1000ms"
-// 	// 3 second relayer timeout works well with these block times
-// 	gaiaTestConfig = testChainConfig{
-// 		dockerfile:     "./setup/Dockerfile.gaiatest",
-// 		timeout:        3 * time.Second,
-// 		rpcPort:        "26657",
-// 		accountPrefix:  "cosmos",
-// 		trustingPeriod: "330h",
-// 	}
+var (
+	// GAIA BLOCK TIMEOUTS are located in the gaia setup script in the
+	// setup directory.
+	// timeout_commit = "1000ms"
+	// timeout_propose = "1000ms"
+	// 3 second relayer timeout works well with these block times
+	gaiaTestConfig = testChainConfig{
+		dockerfile:     "docker/gaiad/Dockerfile",
+		timeout:        3 * time.Second,
+		rpcPort:        "26657",
+		accountPrefix:  "cosmos",
+		trustingPeriod: "330h",
+	}
 
-// 	// AKASH BLOCK TIMEOUTS on jackzampolin/akashtest:master
-// 	// timeout_commit = "1000ms"
-// 	// timeout_propose = "1000ms"
-// 	// 3 second relayer timeout works well with these block times
-// 	// This is built from contrib/Dockerfile.test from the akash repository:
-// 	akashTestConfig = testChainConfig{
-// 		dockerfile:     "./setup/Dockerfile.akashtest",
-// 		timeout:        3 * time.Second,
-// 		rpcPort:        "26657",
-// 		accountPrefix:  "akash",
-// 		trustingPeriod: "330h",
-// 	}
+	// AKASH BLOCK TIMEOUTS on jackzampolin/akashtest:master
+	// timeout_commit = "1000ms"
+	// timeout_propose = "1000ms"
+	// 3 second relayer timeout works well with these block times
+	// This is built from contrib/Dockerfile.test from the akash repository:
+	akashTestConfig = testChainConfig{
+		dockerfile:     "docker/akash/Dockerfile",
+		timeout:        3 * time.Second,
+		rpcPort:        "26657",
+		accountPrefix:  "akash",
+		trustingPeriod: "330h",
+	}
 
-// 	bech32ibcTestConfig = testChainConfig{
-// 		dockerfile:     "./setup/Dockerfile.bech32ibctest",
-// 		timeout:        3 * time.Second,
-// 		rpcPort:        "26657",
-// 		accountPrefix:  "osmo",
-// 		trustingPeriod: "330h",
-// 	}
+	bech32ibcTestConfig = testChainConfig{
+		dockerfile:     "./setup/Dockerfile.bech32ibctest",
+		timeout:        3 * time.Second,
+		rpcPort:        "26657",
+		accountPrefix:  "osmo",
+		trustingPeriod: "330h",
+	}
 
-// 	seeds = []string{SEED1, SEED2}
-// )
+	seeds = []string{SEED1, SEED2}
+)
 
-// type (
-// 	// testChain represents the different configuration options for spinning up a test
-// 	// cosmos-sdk based blockchain
-// 	testChain struct {
-// 		chainID string
-// 		seed    int
-// 		t       testChainConfig
-// 	}
+type (
+	// testChain represents the different configuration options for spinning up a test
+	// cosmos-sdk based blockchain
+	testChain struct {
+		chainID string
+		seed    int
+		t       testChainConfig
+	}
 
-// 	// testChainConfig represents the chain specific docker and codec configurations
-// 	// required.
-// 	testChainConfig struct {
-// 		dockerfile     string
-// 		rpcPort        string
-// 		timeout        time.Duration
-// 		accountPrefix  string
-// 		trustingPeriod string
-// 	}
-// )
+	// testChainConfig represents the chain specific docker and codec configurations
+	// required.
+	testChainConfig struct {
+		dockerfile     string
+		rpcPort        string
+		timeout        time.Duration
+		accountPrefix  string
+		trustingPeriod string
+	}
+)
 
-// // newTestChain generates a new instance of *Chain with a free TCP port configured as the RPC port
-// func newTestChain(t *testing.T, tc testChain) *ry.Chain {
-// 	_, port, err := server.FreeTCPAddr()
-// 	require.NoError(t, err)
-// 	return &ry.Chain{
-// 		Key:            "testkey",
-// 		ChainID:        tc.chainID,
-// 		RPCAddr:        fmt.Sprintf("http://localhost:%s", port),
-// 		AccountPrefix:  tc.t.accountPrefix,
-// 		GasAdjustment:  1.3,
-// 		TrustingPeriod: tc.t.trustingPeriod,
-// 	}
-// }
+// newTestChain generates a new instance of *Chain with a free TCP port configured as the RPC port
+func newTestChain(t *testing.T, tc testChain) *relayer.Chain {
+	_, port, err := server.FreeTCPAddr()
+	require.NoError(t, err)
+	return &relayer.Chain{
+		Key:            "testkey",
+		ChainID:        tc.chainID,
+		RPCAddr:        fmt.Sprintf("http://localhost:%s", port),
+		AccountPrefix:  tc.t.accountPrefix,
+		GasAdjustment:  1.3,
+		TrustingPeriod: tc.t.trustingPeriod,
+	}
+}
