c5504f3a7d031c68e72c49c0db91259ea2eb85f1
Adam Tucker <adamleetucker@outlook.com>
2022-05-17 23:49:38 +0000

    update tests

--- go.mod
+++ go.mod
@@ -18,6 +18,7 @@ require (
 	github.com/stretchr/testify v1.7.1
 	github.com/tendermint/tendermint v0.34.19
 	github.com/tendermint/tm-db v0.6.6
+	go.uber.org/zap v1.21.0
 	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa
 	google.golang.org/grpc v1.45.0
 )
@@ -30,9 +31,12 @@ require (
 	github.com/DataDog/zstd v1.4.5 // indirect
 	github.com/Microsoft/go-winio v0.5.1 // indirect
 	github.com/Nvveen/Gotty v0.0.0-20120604004816-cd527374f1e5 // indirect
+	github.com/ProtonMail/go-crypto v0.0.0-20210428141323-04723f9f07d7 // indirect
 	github.com/Workiva/go-datastructures v1.0.53 // indirect
+	github.com/acomagu/bufpipe v1.0.3 // indirect
 	github.com/armon/go-metrics v0.3.10 // indirect
 	github.com/avast/retry-go/v4 v4.0.3 // indirect
+	github.com/benbjohnson/clock v1.1.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bgentry/speakeasy v0.1.0 // indirect
 	github.com/btcsuite/btcd v0.22.0-beta // indirect
@@ -59,15 +63,22 @@ require (
 	github.com/docker/go-units v0.4.0 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/dvsekhvalnov/jose2go v0.0.0-20200901110807-248326c1351b // indirect
+	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/go-git/gcfg v1.5.0 // indirect
+	github.com/go-git/go-billy/v5 v5.3.1 // indirect
+	github.com/go-git/go-git/v5 v5.4.2 // indirect
 	github.com/go-kit/kit v0.12.0 // indirect
 	github.com/go-kit/log v0.2.0 // indirect
 	github.com/go-logfmt/logfmt v0.5.1 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/godbus/dbus v0.0.0-20190726142602-4481cbc300e2 // indirect
 	github.com/gogo/gateway v1.1.0 // indirect
 	github.com/golang/snappy v0.0.3 // indirect
 	github.com/google/btree v1.0.0 // indirect
+	github.com/google/go-github/v43 v43.0.0 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/orderedcode v0.0.1 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/gorilla/handlers v1.5.1 // indirect
@@ -83,7 +94,10 @@ require (
 	github.com/imdario/mergo v0.3.12 // indirect
 	github.com/improbable-eng/grpc-web v0.14.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
 	github.com/jmhodges/levigo v1.0.0 // indirect
+	github.com/jsternberg/zap-logfmt v1.2.0 // indirect
+	github.com/kevinburke/ssh_config v0.0.0-20201106050909-4977a11b4351 // indirect
 	github.com/keybase/go-keychain v0.0.0-20190712205309-48d3d31d256d // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/lib/pq v1.10.4 // indirect
@@ -114,6 +128,7 @@ require (
 	github.com/rs/cors v1.8.2 // indirect
 	github.com/rs/zerolog v1.23.0 // indirect
 	github.com/sasha-s/go-deadlock v0.2.1-0.20190427202633-1595213edefa // indirect
+	github.com/sergi/go-diff v1.1.0 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -125,6 +140,7 @@ require (
 	github.com/tendermint/btcd v0.1.1 // indirect
 	github.com/tendermint/crypto v0.0.0-20191022145703-50d29ede1e15 // indirect
 	github.com/tendermint/go-amino v0.16.0 // indirect
+	github.com/xanzy/ssh-agent v0.3.0 // indirect
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
@@ -132,7 +148,6 @@ require (
 	go.etcd.io/bbolt v1.3.5 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
 	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
@@ -141,6 +156,7 @@ require (
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
+	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	nhooyr.io/websocket v1.8.6 // indirect
--- test/test_queries.go
+++ test/test_queries.go
@@ -5,63 +5,69 @@ import (
 	"testing"
 	"time"
 
-	"github.com/cosmos/relayer/v2/relayer"
-
-	"github.com/avast/retry-go"
+	"github.com/avast/retry-go/v4"
 	clientypes "github.com/cosmos/ibc-go/v3/modules/core/02-client/types"
+	"github.com/cosmos/relayer/v2/relayer"
 	"github.com/stretchr/testify/require"
 )
 
 // testClientPair tests that the client for src on dst and dst on src are the only clients on those chains
-func testClientPair(t *testing.T, src, dst *relayer.Chain) {
-	testClient(t, src, dst)
-	testClient(t, dst, src)
+func testClientPair(ctx context.Context, t *testing.T, src, dst *relayer.Chain) {
+	t.Helper()
+	testClient(ctx, t, src)
+	testClient(ctx, t, dst)
 }
 
 // testClient queries client for existence of dst on src
-func testClient(t *testing.T, src, dst *relayer.Chain) {
-	srch, err := src.QueryLatestHeight()
+func testClient(ctx context.Context, t *testing.T, src *relayer.Chain) {
+	t.Helper()
+
+	srch, err := src.ChainProvider.QueryLatestHeight(ctx)
 	require.NoError(t, err)
-	var (
-		client *clientypes.QueryClientStateResponse
-	)
-	if err = retry.Do(func() error {
-		_, err = src.QueryClientStateResponse(srch)
+
+	var client *clientypes.QueryClientStateResponse
+	require.NoError(t, retry.Do(func() error {
+		var err error
+		client, err = src.ChainProvider.QueryClientStateResponse(ctx, srch, src.ClientID())
 		if err != nil {
-			srch, _ = src.QueryLatestHeight()
+			srch, _ = src.ChainProvider.QueryLatestHeight(ctx)
+			return err
 		}
-		return err
-	}); err != nil {
-		return
-	}
-	require.NoError(t, err)
+
+		return nil
+	}))
+
 	require.NotNil(t, client)
+
 	cs, err := clientypes.UnpackClientState(client.ClientState)
 	require.NoError(t, err)
-	require.Equal(t, cs.ClientType(), "07-tendermint")
+	require.Equal(t, cs.ClientType(), "07-tendermint") // TODO remove this check or create separate test for substrate
 }
 
 // testConnectionPair tests that the only connection on src and dst is between the two chains
-func testConnectionPair(t *testing.T, src, dst *relayer.Chain) {
-	testConnection(t, src, dst)
-	testConnection(t, dst, src)
+func testConnectionPair(ctx context.Context, t *testing.T, src, dst *relayer.Chain) {
+	t.Helper()
+	testConnection(ctx, t, src, dst)
+	testConnection(ctx, t, dst, src)
 }
 
 // testConnection tests that the only connection on src has a counterparty that is the connection on dst
-func testConnection(t *testing.T, src, dst *relayer.Chain) {
-	conns, err := src.QueryConnections(relayer.DefaultPageRequest())
-	require.NoError(t, err)
-	require.Equal(t, len(conns.Connections), 1)
-	require.Equal(t, conns.Connections[0].ClientId, src.PathEnd.ClientID)
-	require.Equal(t, conns.Connections[0].Counterparty.GetClientID(), dst.PathEnd.ClientID)
-	require.Equal(t, conns.Connections[0].Counterparty.GetConnectionID(), dst.PathEnd.ConnectionID)
-	require.Equal(t, conns.Connections[0].State.String(), "STATE_OPEN")
+func testConnection(ctx context.Context, t *testing.T, src, dst *relayer.Chain) {
+	t.Helper()
 
-	h, err := src.Client.Status(context.Background())
+	conns, err := src.ChainProvider.QueryConnections(ctx)
+	require.NoError(t, err)
+	require.Equal(t, len(conns), 1)
+	require.Equal(t, conns[0].ClientId, src.PathEnd.ClientID)
+	require.Equal(t, conns[0].Counterparty.GetClientID(), dst.PathEnd.ClientID)
+	require.Equal(t, conns[0].Counterparty.GetConnectionID(), dst.PathEnd.ConnectionID)
+	require.Equal(t, conns[0].State.String(), "STATE_OPEN")
+
+	h, err := src.ChainProvider.QueryLatestHeight(ctx)
 	require.NoError(t, err)
 
 	time.Sleep(time.Second * 5)
-	conn, err := src.QueryConnection(h.SyncInfo.LatestBlockHeight)
+	conn, err := src.ChainProvider.QueryConnection(ctx, h, src.ConnectionID())
 	require.NoError(t, err)
 	require.Equal(t, conn.Connection.ClientId, src.PathEnd.ClientID)
 	require.Equal(t, conn.Connection.GetCounterparty().GetClientID(), dst.PathEnd.ClientID)
@@ -70,29 +76,32 @@ func testConnection(t *testing.T, src, dst *relayer.Chain) {
 }
 
 // testChannelPair tests that the only channel on src and dst is between the two chains
-func testChannelPair(t *testing.T, src, dst *relayer.Chain) {
-	testChannel(t, src, dst)
-	testChannel(t, dst, src)
+func testChannelPair(ctx context.Context, t *testing.T, src, dst *relayer.Chain, channelID, portID string) {
+	t.Helper()
+	testChannel(ctx, t, src, dst, channelID, portID)
+	testChannel(ctx, t, dst, src, channelID, portID)
 }
 
 // testChannel tests that the only channel on src is a counterparty of dst
-func testChannel(t *testing.T, src, dst *relayer.Chain) {
-	chans, err := src.QueryChannels(relayer.DefaultPageRequest())
-	require.NoError(t, err)
-	require.Equal(t, 1, len(chans.Channels))
-	require.Equal(t, chans.Channels[0].Ordering.String(), "ORDER_UNORDERED")
-	require.Equal(t, chans.Channels[0].State.String(), "STATE_OPEN")
-	require.Equal(t, chans.Channels[0].Counterparty.ChannelId, dst.PathEnd.ChannelID)
-	require.Equal(t, chans.Channels[0].Counterparty.GetPortID(), dst.PathEnd.PortID)
+func testChannel(ctx context.Context, t *testing.T, src, dst *relayer.Chain, channelID, portID string) {
+	t.Helper()
 
-	h, err := src.Client.Status(context.Background())
+	chans, err := src.ChainProvider.QueryChannels(ctx)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(chans))
+	require.Equal(t, chans[0].Ordering.String(), "ORDER_UNORDERED")
+	require.Equal(t, chans[0].State.String(), "STATE_OPEN")
+	require.Equal(t, chans[0].Counterparty.ChannelId, channelID)
+	require.Equal(t, chans[0].Counterparty.GetPortID(), portID)
+
+	h, err := src.ChainProvider.QueryLatestHeight(ctx)
 	require.NoError(t, err)
 
 	time.Sleep(time.Second * 5)
-	ch, err := src.QueryChannel(h.SyncInfo.LatestBlockHeight)
+	ch, err := src.ChainProvider.QueryChannel(ctx, h, channelID, portID)
 	require.NoError(t, err)
 	require.Equal(t, ch.Channel.Ordering.String(), "ORDER_UNORDERED")
 	require.Equal(t, ch.Channel.State.String(), "STATE_OPEN")
-	require.Equal(t, ch.Channel.Counterparty.ChannelId, dst.PathEnd.ChannelID)
-	require.Equal(t, ch.Channel.Counterparty.GetPortID(), dst.PathEnd.PortID)
+	require.Equal(t, ch.Channel.Counterparty.ChannelId, channelID)
+	require.Equal(t, ch.Channel.Counterparty.GetPortID(), portID)
 }
